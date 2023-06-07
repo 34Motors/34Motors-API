1bc16ac37347a0391fc0c149640b7c9ef577e897
Paulo Moreno <106778522+PauloMorenoD@users.noreply.github.com>
2023-06-07 11:14:31 -0300

    feat : edit car route in routes

--- src/routes/cars.routes.ts
+++ src/routes/cars.routes.ts
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import verifyDataIsValid from "../middlewares/verifyDataIsValid.middleware";
-import { createCarBody } from "../schemas/cars.schema";
+import { carEditSchema, createCarBody } from "../schemas/cars.schema";
 import { createCarController } from "../controllers/cars/createCar.controller";
 import { verifyUserExists } from "../middlewares/verifyUserExists.middleware";
 import { listCarController } from "../controllers/cars/listCar.controller";
+import { editCarController } from "../controllers/cars/editCar.controller";
 
 const carRoutes: Router = Router()
 
 
 carRoutes.post("", verifyDataIsValid(createCarBody), verifyUserExists, createCarController)
 carRoutes.get("/:id", listCarController)
+carRoutes.patch("/:id", verifyDataIsValid(carEditSchema), editCarController)
 
 export { carRoutes }
