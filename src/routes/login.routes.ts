45e60ea4984280ae5cb2afed066544125fd5a593
Olivio Freitas <ojfneto92@gmail.com>
2023-06-12 13:14:25 -0300

    feat: adjust login route

--- src/routes/login.routes.ts
+++ src/routes/login.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import verifyDataIsValid from "../middlewares/verifyDataIsValid.middleware";
 import { loginSchema } from "../schemas/login.schema";
+import { createLoginController } from "../controllers/login/login.controller";
 
 const loginRoutes: Router = Router();
 
-loginRoutes.post("", verifyDataIsValid(loginSchema), /* createLoginController */)
+loginRoutes.post("", verifyDataIsValid(loginSchema), createLoginController)
 
 export { loginRoutes };
