99cd1f800392499a502263b46b6224e2c7e79a76
Olivio Freitas <ojfneto92@gmail.com>
2023-06-13 14:12:40 -0300

    feat: apply cors on top middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
-import "express-async-errors"
+import "express-async-errors";
 import express, { Application } from "express";
+import cors from "cors";
 import { handleErrors } from "./errors";
 import { carRoutes } from "./routes/cars.routes";
 import { loginRoutes } from "./routes/login.routes";
 
 const app: Application = express();
+app.use(cors());
+
 app.use(express.json());
 
-app.use("/cars", carRoutes)
+app.use("/cars", carRoutes);
 app.use("/login", loginRoutes);
 
-app.use(handleErrors)
+app.use(handleErrors);
 export default app;
