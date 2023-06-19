9d3c603ce624b0f457cf894723e5e061042b22af
Olivio Freitas <ojfneto92@gmail.com>
2023-06-19 15:17:25 -0300

    feat: create delete user service

---
+++ src/services/users/deleteUser.service.ts
@@ -0,0 +1,27 @@
+import { prismaClient } from "../../database/index";
+
+const deleteUserService = async (id: number) => {
+  try {
+    const user = await prismaClient.user.findUnique({
+      where: { id },
+      select: { address: true },
+    });
+    const addressId = Number(user?.address?.id);
+
+    const address = await prismaClient.address.findUnique({
+      where: { id: addressId },
+    });
+
+    await prismaClient.address.delete({
+      where: { id: addressId },
+    });
+
+    await prismaClient.user.delete({
+      where: { id },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { deleteUserService };
