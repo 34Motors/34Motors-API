bcfeecec57b4d7cfae865042e812efa0fc5cee25
Olivio Freitas <ojfneto92@gmail.com>
2023-06-19 15:17:49 -0300

    feat: added delete route to user

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,10 +3,13 @@ import { createUserController } from "../controllers/users/createUser.controller
 import verifyDataIsValid from "../middlewares/verifyDataIsValid.middleware";
 import { createUserBody } from "../schemas/users.schema";
 import { listUserController } from "../controllers/users/listUser.controller";
+import verifyIsAuth from "../middlewares/verifyIsAuth.middleware";
+import { deleteUserController } from "../controllers/users/deleteUser.controller";
 
 const userRoutes: Router = Router();
 
 userRoutes.post("", verifyDataIsValid(createUserBody) ,createUserController);
 userRoutes.get("/:id" ,listUserController);
+userRoutes.delete("", verifyIsAuth, deleteUserController);
 
 export { userRoutes };
