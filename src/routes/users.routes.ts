d745c781147653afccc8998c6683ee5b139e929a
Ana Carolina <carol.oliva.cruz@gmail.com>
2023-06-19 21:46:32 -0300

    fix: clean code

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -7,7 +7,6 @@ import {
   userEditSchema,
 } from "../schemas/users.schema";
 import { listUserController } from "../controllers/users/listUser.controller";
-import { verifyUserExists } from "../middlewares/verifyUserExists.middleware";
 import { editUserController } from "../controllers/users/editUser.controller";
 import verifyIsAuth from "../middlewares/verifyIsAuth.middleware";
 import { editAddressController } from "../controllers/address/editAddress.controller";
@@ -17,7 +16,7 @@ import { verifyIsEmpty } from "../middlewares/verifyIsEmpty.middleware";
 const userRoutes: Router = Router();
 
 userRoutes.post("", verifyDataIsValid(createUserBody), createUserController);
-userRoutes.get("/:id", listUserController);
+userRoutes.get("/:id", verifyUserIdForParamsExist, listUserController);
 userRoutes.patch(
   "/:id",
   verifyUserIdForParamsExist,
