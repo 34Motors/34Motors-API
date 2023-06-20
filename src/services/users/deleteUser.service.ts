16314afa7c58621099506d58c22b3ac0d53cb4f5
Olivio Freitas <ojfneto92@gmail.com>
2023-06-20 12:28:05 -0300

    fix: compare password to delete user

--- src/services/users/deleteUser.service.ts
+++ src/services/users/deleteUser.service.ts
@@ -7,11 +7,9 @@ const deleteUserService = async (id: number, passwordData: any) => {
       where: { id },
       select: { password: true, address: true },
     });
-
     if (!user) throw new AppError("User not found", 403);
     
     const passwordMatch: boolean = await compare(passwordData, user.password);
-
     if (!passwordMatch) throw new AppError("Invalid password", 403);
 
     const addressId = Number(user?.address?.id);
