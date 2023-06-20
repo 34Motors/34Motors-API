0a8651626229de0e77f128601d723abba127a5cf
arthurnaocopia@gmail.com <arthurnaocopia@gmail.com>
2023-06-20 12:39:49 -0300

    feat: All cars route now returns filtering options 🔍

--- src/services/cars/listAllCars.service.ts
+++ src/services/cars/listAllCars.service.ts
@@ -1,27 +1,82 @@
-import { prismaClient } from "../../database"
+import { prismaClient } from "../../database";
 
 const listAllCarService = async () => {
+  const cars = await prismaClient.car.findMany({
+    include: {
+      user: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
 
-    const car = await prismaClient.car.findMany({
-        include: {
-            user: {
-                select: {
-                    id: true,
-                    name: true,
-                    email: true,
-                    birthDate: true,
-                    phone: true,
-                    isSeller: true,
-                    description: true,
-                    cpf: true,
-                }
-            },
-            comments: true,
-            images: true,
-        }
-    })
+  const filters = await avaliableFilters();
 
-    return car
+  return {
+    filters,
+    cars,
+  };
+};
+
+async function avaliableFilters() {
+  const prismaBrands = await prismaClient.car.groupBy({
+    by: ["brand"],
+    _count: {
+      brand: true,
+    },
+  });
+
+  const prismaColors = await prismaClient.car.groupBy({
+    by: ["color"],
+    _count: {
+      color: true,
+    },
+  });
+
+  const prismaYears = await prismaClient.car.groupBy({
+    by: ["year"],
+    _count: {
+      year: true,
+    },
+  });
+
+  const prismaFuelTypes = await prismaClient.car.groupBy({
+    by: ["fuelType"],
+    _count: {
+      fuelType: true,
+    },
+  });
+
+  const prices = await prismaClient.car.aggregate({
+    _min: { price: true },
+    _max: { price: true },
+  });
+
+  const quilometers = await prismaClient.car.aggregate({
+    _min: { quilometers: true },
+    _max: { quilometers: true },
+  });
+
+  const brands = prismaBrands.map((item) => item.brand);
+  const colors = prismaColors.map((item) => item.color);
+  const years = prismaYears.map((item) => item.year);
+  const fuelTypes = prismaFuelTypes.map((item) => item.fuelType);
+  const maxPrice = prices._max.price;
+  const minPrice = prices._min.price;
+  const maxQuilometers = quilometers._max.quilometers;
+  const minQuilometers = quilometers._min.quilometers;
+
+  return {
+    maxPrice,
+    minPrice,
+    maxQuilometers,
+    minQuilometers,
+    brands,
+    colors,
+    years,
+    fuelTypes,
+  };
 }
 
-export { listAllCarService }
+export { listAllCarService };
