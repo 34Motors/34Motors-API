e7ce70ca5153410f2bd6ebd164a117cdd636a7e9
Arthur Vinícius <arthurnaocopia@gmail.com>
2023-06-22 18:31:47 -0300

    Feat: Ultra dynamic mega optimized filtering options 🔍

--- src/services/cars/listAllCars.service.ts
+++ src/services/cars/listAllCars.service.ts
@@ -1,8 +1,10 @@
+import { Car } from "@prisma/client";
 import { prismaClient } from "../../database";
 import { Request } from "express";
 
-interface filter {
+interface iFilter {
   brand: string;
+  model: string;
   color: string;
   year: string;
   fuelType: "Flex" | "Hibrido" | "Eletrico";
@@ -15,12 +17,26 @@ interface filter {
     gte: number;
   };
 }
+interface iFilterOptions {
+  brand: string[];
+  model: string[];
+  color: string[];
+  year: string[];
+  fuelType: string[];
+  price: {
+    max: number;
+    min: number;
+  };
+  quilometers: {
+    max: number;
+    min: number;
+  };
+}
 
 const listAllCarService = async (queryParams: Request["query"]) => {
-  const filterOptions = await avaliableFilters();
-
   const {
     brand,
+    model,
     color,
     year,
     fuelType,
@@ -30,38 +46,66 @@ const listAllCarService = async (queryParams: Request["query"]) => {
     maxQuilometers,
   } = queryParams;
 
-  const filters = {} as filter;
+  let refine = false;
+
+  if (
+    brand ||
+    model ||
+    color ||
+    year ||
+    fuelType ||
+    maxPrice ||
+    minPrice ||
+    minQuilometers ||
+    maxQuilometers
+  ) {
+    refine = true;
+  }
+
+  let filterOptions = await avaliableFilters();
+
+  const filters = {} as iFilter;
 
   if (brand) {
-    filters.brand = brand as filter["brand"];
+    filters.brand = brand as iFilter["brand"];
+    filterOptions.brand = [brand as string];
+  }
+
+  if (model) {
+    filters.model = model as iFilter["model"];
+    filterOptions.model = [model as string];
   }
 
   if (color) {
-    filters.color = color as filter["color"];
+    filters.color = color as iFilter["color"];
+    filterOptions.color = [color as string];
   }
 
   if (year) {
-    filters.year = year as filter["year"];
+    filters.year = year as iFilter["year"];
+    filterOptions.year = [year as string];
   }
 
   if (fuelType) {
-    filters.fuelType = fuelType as filter["fuelType"];
+    filters.fuelType = fuelType as iFilter["fuelType"];
+    filterOptions.fuelType = [fuelType as string];
   }
 
   filters.price = {
-    lte: +(maxPrice || filterOptions.maxPrice),
-    gte: +(minPrice || filterOptions.minPrice),
+    lte: +(maxPrice || filterOptions.price.max),
+    gte: +(minPrice || filterOptions.price.min),
   };
 
   filters.quilometers = {
-    lte: +(maxQuilometers || filterOptions.maxQuilometers),
-    gte: +(minQuilometers || filterOptions.minQuilometers),
+    lte: +(maxQuilometers || filterOptions.quilometers.max),
+    gte: +(minQuilometers || filterOptions.quilometers.min),
   };
 
   const cars = await prismaClient.car.findMany({
     include: {
       user: {
         select: {
+          id: true,
           name: true,
         },
       },
@@ -69,15 +113,26 @@ const listAllCarService = async (queryParams: Request["query"]) => {
     where: filters,
   });
 
+  if (refine) {
+    filterOptions = refineFilterOptions(cars as Car[]);
+  }
+
   return {
     filterOptions,
     cars,
   };
 };
 
-async function avaliableFilters() {
+async function avaliableFilters(): Promise<iFilterOptions> {
   const prismaBrands = await prismaClient.car.groupBy({
     by: ["brand"],
+    _count: {
+      brand: true,
+    },
+  });
+
+  const prismaModels = await prismaClient.car.groupBy({
+    by: ["model"],
     _count: {
       brand: true,
     },
@@ -114,25 +169,110 @@ async function avaliableFilters() {
     _max: { quilometers: true },
   });
 
-  const brands = prismaBrands.map((item) => item.brand);
-  const colors = prismaColors.map((item) => item.color);
-  const years = prismaYears.map((item) => item.year);
-  const fuelTypes = prismaFuelTypes.map((item) => item.fuelType);
+  const brand = prismaBrands.map((item) => item.brand);
+  const model = prismaModels.map((item) => item.model);
+  const color = prismaColors.map((item) => item.color);
+  const year = prismaYears.map((item) => item.year);
+  const fuelType = prismaFuelTypes.map((item) => item.fuelType);
   const maxPrice = prices._max.price || 0;
   const minPrice = prices._min.price || 0;
   const maxQuilometers = quilometers._max.quilometers || 0;
   const minQuilometers = quilometers._min.quilometers || 0;
 
   return {
-    maxPrice,
-    minPrice,
-    maxQuilometers,
-    minQuilometers,
-    brands,
-    colors,
-    years,
-    fuelTypes,
+    brand,
+    model,
+    color,
+    year,
+    fuelType,
+    price: { max: maxPrice, min: minPrice },
+    quilometers: { max: maxQuilometers, min: minQuilometers },
   };
+}
+
+function refineFilterOptions(carros: Car[]) {
+  const opcoesFiltragem: iFilterOptions = {
+    brand: [],
+    model: [],
+    color: [],
+    year: [],
+    fuelType: [],
+    price: {
+      max: 0,
+      min: Number.MAX_SAFE_INTEGER,
+    },
+    quilometers: {
+      max: 0,
+      min: Number.MAX_SAFE_INTEGER,
+    },
+  };
+
+  for (let carro of carros) {
+    if (carro.brand) {
+      opcoesFiltragem.brand = opcoesFiltragem.brand || [];
+      if (!opcoesFiltragem.brand.includes(carro.brand)) {
+        opcoesFiltragem.brand.push(carro.brand);
+      }
+    }
+
+    if (carro.model) {
+      opcoesFiltragem.model = opcoesFiltragem.model || [];
+      if (!opcoesFiltragem.model.includes(carro.model)) {
+        opcoesFiltragem.model.push(carro.model);
+      }
+    }
+
+    if (carro.color) {
+      opcoesFiltragem.color = opcoesFiltragem.color || [];
+      if (!opcoesFiltragem.color.includes(carro.color)) {
+        opcoesFiltragem.color.push(carro.color);
+      }
+    }
+
+    if (carro.year) {
+      opcoesFiltragem.year = opcoesFiltragem.year || [];
+      if (!opcoesFiltragem.year.includes(carro.year)) {
+        opcoesFiltragem.year.push(carro.year);
+      }
+    }
+
+    if (carro.fuelType) {
+      opcoesFiltragem.fuelType = opcoesFiltragem.fuelType || [];
+      if (!opcoesFiltragem.fuelType.includes(carro.fuelType)) {
+        opcoesFiltragem.fuelType.push(carro.fuelType);
+      }
+    }
+
+    if (carro.price) {
+      opcoesFiltragem.price.max = Math.max(
+        opcoesFiltragem.price.max || 0,
+        carro.price
+      );
+    }
+
+    if (carro.price) {
+      opcoesFiltragem.price.min = Math.min(
+        opcoesFiltragem.price.min || Number.MAX_SAFE_INTEGER,
+        carro.price
+      );
+    }
+
+    if (carro.quilometers) {
+      opcoesFiltragem.quilometers.min = Math.min(
+        opcoesFiltragem.quilometers.min || Number.MAX_SAFE_INTEGER,
+        carro.quilometers
+      );
+    }
+
+    if (carro.quilometers) {
+      opcoesFiltragem.quilometers.max = Math.max(
+        opcoesFiltragem.quilometers.max || 0,
+        carro.quilometers
+      );
+    }
+  }
+
+  return opcoesFiltragem;
 }
 
 export { listAllCarService };
